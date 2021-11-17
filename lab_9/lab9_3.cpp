725de9038bbe3189d700ef1f522e884e57d8d5af
artttnik <artttnik@gmail.com>
2021-11-17 22:31:27 +0300

    lab9_3 fix

--- lab_9/lab9_3.cpp
+++ lab_9/lab9_3.cpp
@@ -1,30 +1,57 @@
-#include <iostream.h>
-#include <stdio.h>
-#include <conio.h>
 #include <dos.h>
-#include <bios.h>
-#define Port8042 0x60
+#include <conio.h>
+#include <stdio.h>
 
-int main()
-{
-	clrscr();
-	char symb;
-	symb = getchar();
-	while (symb != 'q')
-	{
-		if (symb == 'g' || symb == 'G')
-		{
-			outp(0x60, 0xED);
-			for (int i = 0; i < 4000; i++);
-			outp(0x60, 0x04);
-		}
-		if (symb == 'h' || symb == 'H')
-		{
-			outp(0x60, 0xED);
-			for (int i = 0; i < 4000; i++);
-			outp(0x60, 0x02);
-		}
-		symb = getchar();
-	}
-	return 0;
+void interrupt (*old_int9)(...);
+volatile unsigned char scan_code = 0;
+
+void interrupt new_int9(...) {
+    unsigned char code = inp(0x60);
+    scan_code = code;
+    (*old_int9)();
+
+    outp(0x20, 0x20);
+}
+
+int main() {
+    clrscr();
+    printf("Press keys to see scan codes and status\n");
+    printf("Press SPACE to exit\n\n");
+
+    disable();
+    old_int9 = getvect(9);
+    setvect(9, new_int9);
+    enable();
+
+    while (1) {
+        if (scan_code != 0) {
+            unsigned char status1, status2;
+
+            asm {
+                push ds
+                mov  ax, 0x40
+                mov  ds, ax
+                mov  si, 0x17
+                mov  bx, [si]
+                pop  ds
+            }
+
+            status1 = _BL;
+            status2 = _BH;
+
+            printf("Scan = %02X   Status1 = %02X  Status2 = %02X\n",
+                   scan_code, status1, status2);
+
+            scan_code = 0;
+        }
+
+        if (kbhit() && getch() == ' ') break;
+    }
+
+    disable();
+    setvect(9, old_int9);
+    enable();
+
+    printf("\nRestored old interrupt handler. Exiting...\n");
+    return 0;
 }
