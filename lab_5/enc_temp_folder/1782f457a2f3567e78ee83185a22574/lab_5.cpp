e2d814e436d21a81174849b501e27c223880ccec
artttnik <artttnik@gmail.com>
2025-10-06 15:02:27 +0300

    new

---
+++ lab_5/enc_temp_folder/1782f457a2f3567e78ee83185a22574/lab_5.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+using namespace std;
+
+long long int shift_stack(long long int a, long long int b, long long int& result)
+{
+
+	__asm
+	{
+		mov eax, [ebp + 8]
+		mov ebx, [ebp + 16]
+		mul ebx
+		mov esi, [ebp + 24]
+		mov[esi], eax
+		mov[esi + 4], edx
+
+		mov eax, [ebp + 8]
+		mov ebx, [ebp + 20]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		mov eax, [ebp + 12]
+		mov ebx, [ebp + 16]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		mov eax, [ebp + 12]
+		mov ebx, [ebp + 20]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+	}
+	return result;
+}
+
+void shift_reg()
+{
+	__asm
+	{
+		push edx
+		push ebx
+
+		mov eax, [ebx]
+		mov ecx, [edx]
+		mul ecx
+		mov[esi], eax
+		mov[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx]
+		mov ebx, [edx + 4]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx + 4]
+		mov ebx, [edx]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx + 4]
+		mov ebx, [edx + 4]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+
+	}
+}
+
+
+long long int globA = 0;
+long long int globB = 0;
+long long int* globResult = 0;
+
+void shift_global()
+{
+	__asm
+	{
+		lea ebx, globA
+		lea edx, globB
+		mov esi, globResult
+		push edx
+		push ebx
+		mov eax, [ebx]
+		mov ecx, [edx]
+		mul ecx
+		mov[esi], eax
+		mov[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx]
+		mov ebx, [edx + 4]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx + 4]
+		mov ebx, [edx]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+		push edx
+		push ebx
+
+		mov eax, [ebx + 4]
+		mov ebx, [edx + 4]
+		mul ebx
+		adc[esi], eax
+		adc[esi + 4], edx
+
+		pop edx
+		pop ebx
+
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const long long int a = 987654321;
+	const long long int b = 123456789;
+	
+	long long int result = 0;
+
+
+	long long int result1 = shift_stack(a, b, result);
+
+	cout << "Умножение через стек: " << result1 << endl;
+	result = 0;
+
+	__asm
+	{
+		lea ebx, a
+		lea edx, b
+		lea esi, result
+	}
+	shift_reg();
+	cout << "Умножение через регистры: " << result << endl;
+	result = 0;
+
+
+	globA = a;
+	globB = b;
+	globResult = &result;
+	shift_global();
+	cout << "Умножение через глобальные переменные: " << result << endl;
+	
+
+
+	return 0;
+}
--- lab_5/lab_5/lab_5.cpp
+++ lab_5/lab_5/lab_5.cpp
@@ -1,177 +1,48 @@
 ﻿#include <iostream>
 using namespace std;
 
-void shift_stack(long long int a, long long int b, long long int& result)
-{
-	__asm
-	{
-		mov eax, [ebp + 8]
-		mov ebx, [ebp + 16]
-		mul ebx
-		mov esi, [ebp + 24]
-		mov[esi], eax
-		mov[esi + 4], edx
+unsigned int rotate(unsigned int value, unsigned int count, int dir) {
+    unsigned int result = 0;
+    int zf = 0;
+    int cf = 0;
 
-		mov eax, [ebp + 8]
-		mov ebx, [ebp + 20]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
+    __asm {
+        mov ecx, count
+        mov eax, value
+        cmp dir, 0
+        jne rotate_right
 
-		mov eax, [ebp + 12]
-		mov ebx, [ebp + 16]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
+        rotate_left :
+        rol eax, cl
+            jmp done
 
-		mov eax, [ebp + 12]
-		mov ebx, [ebp + 20]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-	}
+            rotate_right :
+        ror eax, cl
+
+            done :
+        mov result, eax
+
+            test eax, eax
+            sete zf
+
+            setc cf
+    }
+
+    cout << "ZF = " << zf << ", CF = " << cf << endl;
+    return result;
 }
 
-void shift_reg()
-{
-	__asm
-	{
-		push edx
-		push ebx
+int main() {
+    unsigned int x = 0x91;
 
-		mov eax, [ebx]
-		mov ecx, [edx]
-		mul ecx
-		mov[esi], eax
-		mov[esi + 4], edx
+    cout << hex;
+    cout << "Initial: " << x << endl;
 
-		pop edx
-		pop ebx
-		push edx
-		push ebx
+    unsigned int res1 = rotate(x, 1, 0);
+    cout << "After rotate left: " << res1 << endl;
 
-		mov eax, [ebx]
-		mov ebx, [edx + 4]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
+    unsigned int res2 = rotate(x, 1, 1);
+    cout << "After rotate right: " << res2 << endl;
 
-		pop edx
-		pop ebx
-		push edx
-		push ebx
-
-		mov eax, [ebx + 4]
-		mov ebx, [edx]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-
-		pop edx
-		pop ebx
-		push edx
-		push ebx
-
-		mov eax, [ebx + 4]
-		mov ebx, [edx + 4]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-
-		pop edx
-		pop ebx
-
-	}
-}
-
-
-long long int globA = 0;
-long long int globB = 0;
-long long int* globResult = 0;
-
-void shift_global()
-{
-	__asm
-	{
-		lea ebx, globA
-		lea edx, globB
-		mov esi, globResult
-		push edx
-		push ebx
-		mov eax, [ebx]
-		mov ecx, [edx]
-		mul ecx
-		mov[esi], eax
-		mov[esi + 4], edx
-
-		pop edx
-		pop ebx
-		push edx
-		push ebx
-
-		mov eax, [ebx]
-		mov ebx, [edx + 4]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-
-		pop edx
-		pop ebx
-		push edx
-		push ebx
-
-		mov eax, [ebx + 4]
-		mov ebx, [edx]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-
-		pop edx
-		pop ebx
-		push edx
-		push ebx
-
-		mov eax, [ebx + 4]
-		mov ebx, [edx + 4]
-		mul ebx
-		adc[esi], eax
-		adc[esi + 4], edx
-
-		pop edx
-		pop ebx
-
-	}
-}
-
-
-int main()
-{
-	setlocale(LC_ALL, "rus");
-	long long int a = 987654321;
-	long long int b = 123456789;
-	long long int result = 0;
-	shift_stack(a, b, result);
-	cout << "Умножение через стек: " << result << endl;
-	result = 0;
-
-	__asm
-	{
-		lea ebx, a
-		lea edx, b
-		lea esi, result
-	}
-	shift_reg();
-	cout << "Умножение через регистры: " << result << endl;
-	result = 0;
-
-
-	globA = a;
-	globB = b;
-	globResult = &result;
-	shift_global();
-	cout << "Умножение через глобальные переменные: " << result << endl;
-	
-
-
-	return 0;
+    return 0;
 }
