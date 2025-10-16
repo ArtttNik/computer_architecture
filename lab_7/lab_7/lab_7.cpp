7ed1d3ee06c748dc8d7f42dd4813d7ef6ca4ed48
artttnik <artttnik@gmail.com>
2025-10-16 22:10:06 +0300

    init for lab_7

---
+++ lab_7/lab_7/lab_7.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	float a = 1;
+	float b = 3;
+	float c = 5;
+	float sum = 0;
+
+	__asm
+	{
+		finit
+		fld a
+		fld b
+		fld c
+		fadd
+		fadd
+		fstp sum
+
+	}
+	cout << "1. sum = " << sum << endl;
+
+	__asm
+	{
+		finit
+		fstp sum
+	}
+	cout << "2. sum = " << sum << endl;
+
+	int ctrl = 0x027F;
+	a = 5;
+	b = 0;
+
+	__asm
+	{
+		finit
+		fldcw ctrl
+		fld a
+		fld b
+		fdiv
+		fstp sum
+	}
+
+	cout << "3. sum = " << sum << endl;
+	return 0;
+}
